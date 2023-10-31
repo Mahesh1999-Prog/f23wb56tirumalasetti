9d2521e2c832383df89652e28d7fa60358277327
Mahesh1999-Prog <143574719+Mahesh1999-Prog@users.noreply.github.com>
2023-10-31 11:45:32 -0500

    Setting up to display results UP1

--- routes/fruit.js
+++ routes/fruit.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('fruit', { title: 'Mahesh Tirumalasetti' });
+  res.render('fruit', { title: 'Fruit Class' });
 });
 
 module.exports = router;
